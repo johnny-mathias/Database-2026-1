a09d9044ded776f7bf669b8661bbebac5aa3865b
johnny-mathias <johnnydiasmathiasjunior@gmail.com>
2026-03-12 19:11:46 -0300

    fix: herois.sql now is PL/SQL command

--- sql/herois.sql
+++ sql/herois.sql
@@ -1,16 +1,32 @@
-CREATE TABLE TB_HEROIS (
-    id_heroi NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    nome VARCHAR2(50),
-    classe VARCHAR2(20),
-    hp_atual NUMBER,
-    hp_max NUMBER,
-    status VARCHAR2(20) DEFAULT 'ATIVO'
-);
+DECLARE
 
-INSERT INTO TB_HEROIS (nome, classe, hp_atual, hp_max) 
-VALUES ('Artorias', 'GUERREIRO', 100, 100);
-INSERT INTO TB_HEROIS (nome, classe, hp_atual, hp_max) 
-VALUES ('Sif', 'LADRÃO', 80, 80);
-INSERT INTO TB_HEROIS (nome, classe, hp_atual, hp_max) 
-VALUES ('Gwyn', 'MAGO', 60, 60);
-COMMIT;
+    v_dano_nevoa NUMBER := 10;
+
+    CURSOR c_herois IS
+        SELECT id_heroi, hp_atual, hp_max
+        FROM TB_HEROIS
+        WHERE status = 'ATIVO'
+        FOR UPDATE;
+
+    v_hp NUMBER;
+
+BEGIN
+
+    FOR r IN c_herois LOOP
+
+        v_hp := r.hp_atual - v_dano_nevoa;
+
+        IF v_hp <= 0 THEN
+            UPDATE TB_HEROIS
+            SET hp_atual = 0,
+                status = 'CAIDO'
+            WHERE id_heroi = r.id_heroi;
+        ELSE
+            UPDATE TB_HEROIS
+            SET hp_atual = v_hp
+            WHERE id_heroi = r.id_heroi;
+        END IF;
+
+    END LOOP;
+
+END;
